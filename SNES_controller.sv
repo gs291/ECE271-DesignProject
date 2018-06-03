4e6b620ccfd2c359d851ef27d8e46d14bc3d98ea
gs291 <sanchez.gregory886@gmail.com>
2018-06-03 15:42:41 -0700

    Add comments to mux

--- SNES_controller.sv
+++ SNES_controller.sv
@@ -1,3 +1,16 @@
+/***************************************
+* Affiliation: Oregon State University
+* Author: Gregory Sanchez
+*
+* Course: ECE 271 - Digital Logic Design
+* Project Name: ECE 271 Final Project
+* Team Number: 09
+*
+* Description: Button Board Decoder
+* Start Date: 06/03/2018
+*
+****************************************/
+
 module mux2( 	input logic [15:0] keyboard_data, button_data,
 				input logic sel,
 				output logic [15:0] SNES_data);
