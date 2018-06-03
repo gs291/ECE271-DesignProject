e49b56564c3c19a3904fd0c24d0e4c84fb8f072a
gs291 <sanchez.gregory886@gmail.com>
2018-06-03 15:40:09 -0700

    Add SNES multiplexer

--- SNES_controller.sv
+++ SNES_controller.sv
@@ -1,4 +1,12 @@
-module mux2( 	input logic [15:0] keyboard_data, button_ 
-				input logic [15:0] button_data,
+module mux2( 	input logic [15:0] keyboard_data, button_data,
 				input logic sel,
 				output logic [15:0] SNES_data);
+				
+always_comb
+	begin
+		case(sel)
+			1'b0: SNES_data = keyboard_data;
+			1'b1: SNES_data = button_data;
+			
+			default: SNES_data = 0;
+	end
