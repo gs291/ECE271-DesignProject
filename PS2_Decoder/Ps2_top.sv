0609c8ac15c05e60b0b41c0bfb1578f5f845b2f6
codeman5199 <32248973+codeman5199@users.noreply.github.com>
2018-06-03 16:06:18 -0700

    Put ps2 top in right place

---
+++ PS2_Decoder/Ps2_top.sv
@@ -0,0 +1,18 @@
+/***************************************
+* Affiliation: Oregon State University
+* Author: Cody McCall
+*
+* Course: ECE 271 - Digital Logic Design
+* Project Name: ECE 271 Design Project
+* Team Number: 09
+*
+* Description: Ps2 Keyboard Top Module
+* Start Date: 06/03/2018
+*
+****************************************/
+module ps2_Top(
+input logic ps2_data, clk_15khz,
+output logic [15:0] ps2_data);
+
+	
+endmodule 
