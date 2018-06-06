c464bdcd0b092f70be5f2b31ebfc31ec433f006d
HaolinLiu <35446504+HaolinLiu@users.noreply.github.com>
2018-06-05 18:59:52 -0700

    Debug ps2_decoder

--- PS2_Decoder/PS2_Decoder.sv
+++ PS2_Decoder/PS2_Decoder.sv
@@ -15,24 +15,24 @@ module Ps2_decoder(
     input logic clk,
 
     output logic [15:0] key_data_out
-)
+);
 
-logic key_fo;
+logic [7:0] key_fo;
 
 always_comb 
-    if(key_byte == F0)
+    if(key_byte == 8'hf0)
         key_fo = 1;
     else if (key_fo == 1)
         begin
             case(key_byte)
-                8'h1d: key_ata_out[0] = 1'b1; // w for up
-                8'h24: key_ata_out[1] = 1'b1; // e for A
-                8'h2D: key_ata_out[2] = 1'b1; // r for B
-                8'h2C: key_ata_out[3] = 1'b1; // t fro start
-                8'h1C: key_ata_out[4] = 1'b1; // a for left
-                8'h1B: key_ata_out[5] = 1'b1; // s for down
-                8'h23: key_ata_out[6] = 1'b1; // d for right
-                8'h2B: key_ata_out[7] = 1'b1; // f fro select 
+                8'h1d: key_data_out[0] = 1'b1; // w for up
+                8'h24: key_data_out[1] = 1'b1; // e for A
+                8'h2D: key_data_out[2] = 1'b1; // r for B
+                8'h2C: key_data_out[3] = 1'b1; // t fro start
+                8'h1C: key_data_out[4] = 1'b1; // a for left
+                8'h1B: key_data_out[5] = 1'b1; // s for down
+                8'h23: key_data_out[6] = 1'b1; // d for right
+                8'h2B: key_data_out[7] = 1'b1; // f fro select 
             endcase
 
             key_fo = 0;
@@ -41,14 +41,14 @@ always_comb
     else
         begin
             case(key_byte)
-                8'h1d: key_ata_out[0] = 1'b0; // w for up
-                8'h24: key_ata_out[1] = 1'b0; // e for A
-                8'h2D: key_ata_out[2] = 1'b0; // r for B
-                8'h2C: key_ata_out[3] = 1'b0; // t fro start
-                8'h1C: key_ata_out[4] = 1'b0; // a for left
-                8'h1B: key_ata_out[5] = 1'b0; // s for down
-                8'h23: key_ata_out[6] = 1'b0; // d for right
-                8'h2B: key_ata_out[7] = 1'b0; // f fro select
+                8'h1d: key_data_out[0] = 1'b0; // w for up
+                8'h24: key_data_out[1] = 1'b0; // e for A
+                8'h2D: key_data_out[2] = 1'b0; // r for B
+                8'h2C: key_data_out[3] = 1'b0; // t fro start
+                8'h1C: key_data_out[4] = 1'b0; // a for left
+                8'h1B: key_data_out[5] = 1'b0; // s for down
+                8'h23: key_data_out[6] = 1'b0; // d for right
+                8'h2B: key_data_out[7] = 1'b0; // f fro select
             endcase
         end
 
