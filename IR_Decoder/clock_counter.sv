b93512f82c1f783213e9fe029a154739f5a9287e
mattguo5 <39818100+mattguo5@users.noreply.github.com>
2018-06-07 16:26:06 -0700

    Add files via upload

--- IR_Decoder/clock_counter.sv
+++ IR_Decoder/clock_counter.sv
@@ -1,18 +1,28 @@
+/***************************************
+* Affiliation: Oregon State University
+* Author: Matt Guo
+*
+* Course: ECE 271 - Digital Logic Design
+* Project Name: ECE 271 Final Project
+* Team Number: 09
+*
+* Description: Clock Counter for IR State Machine
+* Start Date: 06/05/2018
+*
+****************************************/
+
+
 module clock_counter(
 
-	input logic reset,
-	output logic slow_clk
+	input logic reset,			//takes in active high reset from top module
+	input logic clk_in,			//takes in 2.08MHz clock signal from top module
+	output logic slow_clk		//outputs 16kHz clock signal to IR State Machine
 );
 
-	logic [7:0] clock_counter;
+	logic [7:0] clock_counter;	//internal clock counter
 	
-	OSCH #("2.08") osc_int (	
-		.STDBY(1'b0),			
-		.OSC(clk),				
-		.SEDSTDBY()				
-	);	
 	
-	always_ff @ (posedge clk, posedge reset)
+	always_ff @ (posedge clk_in, posedge reset)
 	begin
 		if(reset) clock_counter <= 0;
 		else	  clock_counter <= clock_counter + 1;
