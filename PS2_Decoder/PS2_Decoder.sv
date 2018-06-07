39a6e0bbaf6dddd8c9a156abac56fe648a087e5f
gs291 <sanchez.gregory886@gmail.com>
2018-06-06 22:30:10 -0700

    Update PS2_Decoder for SNES output

--- PS2_Decoder/PS2_Decoder.sv
+++ PS2_Decoder/PS2_Decoder.sv
@@ -12,43 +12,31 @@
 ****************************************/
 module Ps2_decoder(
     input logic [7:0] key_byte,
-    input logic clk,
 
     output logic [15:0] key_data_out
 );
 
-<<<<<<< HEAD
-logic key_fo;
-=======
-logic key_fo, start_key;
->>>>>>> e4b7f01f2617dba9263e654e9e253d017ad28f95
 
-logic initialize;
+logic key_fo; //Break code enable for Hexideciaml F0 (1111_0000)
 
 always_comb
-    if(initialize != 0)
-      begin
-        key_data_out = 65535;
-        initialize = 0;
-      end
-    else if(key_byte == 8'hf0)
+  if(key_byte == 8'hf0)
         key_fo = 1;
     else if (key_fo == 1)
         begin
-            //key_data_out = 65535; //Assigns output to all HIGH Values
-
-            //The SNES console requires the selected button to be LOW
+            //The button that was let go will be assigned to HIGH
+            //if the break code was sent prior to this key_byte
             case(key_byte)
-                8'h1d: key_data_out[0] = 1'b1; // w for up
-                8'h24: key_data_out[1] = 1'b1; // e for A
-                8'h2D: key_data_out[2] = 1'b1; // r for B
-                8'h2C: key_data_out[3] = 1'b1; // t for start
-                8'h1C: key_data_out[4] = 1'b1; // a for left
-                8'h1B: key_data_out[5] = 1'b1; // s for down
-                8'h23: key_data_out[6] = 1'b1; // d for right
-                8'h2B: key_data_out[7] = 1'b1; // f for select
-                8'h34: key_data_out[8] = 1'b1; // g for X
-                8'h35: key_data_out[9] = 1'b1; // y for Y
+                8'h2D: key_data_out[15] = 1'b1; // r for B
+                8'h35: key_data_out[14] = 1'b1; // y for Y
+                8'h2B: key_data_out[13] = 1'b1; // f for select
+                8'h2C: key_data_out[12] = 1'b1; // t for start
+                8'h1d: key_data_out[11] = 1'b1; // w for up
+                8'h1B: key_data_out[10] = 1'b1; // s for down
+                8'h1C: key_data_out[9] = 1'b1; // a for left
+                8'h23: key_data_out[8] = 1'b1; // d for right
+                8'h24: key_data_out[7] = 1'b1; // e for A
+                8'h34: key_data_out[6] = 1'b1; // g for X
             endcase
 
             key_fo = 0;
@@ -56,20 +44,18 @@ always_comb
         end
     else
         begin
-            //key_data_out = 65535; //Assigns output to all HIGH Values
-
             //The SNES console requires the selected button to be LOW
             case(key_byte)
-                8'h1d: key_data_out[0] = 1'b0; // w for up
-                8'h24: key_data_out[1] = 1'b0; // e for A
-                8'h2D: key_data_out[2] = 1'b0; // r for B
-                8'h2C: key_data_out[3] = 1'b0; // t for start
-                8'h1C: key_data_out[4] = 1'b0; // a for left
-                8'h1B: key_data_out[5] = 1'b0; // s for down
-                8'h23: key_data_out[6] = 1'b0; // d for right
-                8'h2B: key_data_out[7] = 1'b0; // f for select
-                8'h34: key_data_out[8] = 1'b0; // g for X
-                8'h35: key_data_out[9] = 1'b0; // y for Y
+                8'h2D: key_data_out[15] = 1'b0; // r for B
+                8'h35: key_data_out[14] = 1'b0; // y for Y
+                8'h2B: key_data_out[13] = 1'b0; // f for select
+                8'h2C: key_data_out[12] = 1'b0; // t for start
+                8'h1d: key_data_out[11] = 1'b0; // w for up
+                8'h1B: key_data_out[10] = 1'b0; // s for down
+                8'h1C: key_data_out[9] = 1'b0; // a for left
+                8'h23: key_data_out[8] = 1'b0; // d for right
+                8'h24: key_data_out[7] = 1'b0; // e for A
+                8'h34: key_data_out[6] = 1'b0; // g for X
                 default: key_data_out = 16'b1111111111111111;
             endcase
         end
