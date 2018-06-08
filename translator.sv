95a444a33477da287a696d288f91e62f656795ab
gs291 <sanchez.gregory886@gmail.com>
2018-06-07 18:07:07 -0700

    Update translator sv and do file

--- translator.sv
+++ translator.sv
@@ -1,22 +1,46 @@
+/***************************************
+* Affiliation: Oregon State University
+* Author: Gregory Sanchez
+*
+* Course: ECE 271 - Digital Logic Design
+* Project Name: ECE 271 Final Project
+* Team Number: 09
+*
+* Description: Serially outputs the button input(s) to
+*               to the SNES console using a shift register
+* Start Date: 06/03/2018
+*
+****************************************/
 module translator(input logic [15:0] data,
                   input logic SNES_latch,
                   input logic reset,
                   input logic SNES_clk,
-                  output logic SNES_data
-);
+                  output logic SNES_data);
 
-                  logic [15:0] shift_register;
+logic send_data;
+logic [3:0] count;
 
-always_ff @(negedge SNES_clk, posedge SNES_latch, posedge reset)
-	begin
-		if(reset)
-			shift_register <= 0;
-		else if (SNES_latch)
-			shift_register <= data;
-		else
-			shift_register <= {shift_register[14:0], 1'bz};
+always_ff @ (posedge SNES_latch)
+if(SNES_latch)
+  begin
+    send_data <= 1'b1;
+    count <= 15;
+  end
 
-		assign SNES_data = shift_register[15];
-	end
-	
+always_ff @(negedge SNES_clk,posedge reset)
+begin
+  if(reset)
+    count <= 0;
+	else if(send_data)
+    begin
+      case(count)
+        0: SNES_data <= data[count];
+        default:
+          begin
+          SNES_data <= data[count];
+          count <= count - 1;
+          end
+      endcase
+    end
+end
 endmodule
