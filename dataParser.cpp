44517714f7fb8956cd986ed1821fea23f8233e5a
gs291 <sanchez.gregory886@gmail.com>
2018-06-07 18:42:35 -0700

    Update dataParser file

--- dataParser.cpp
+++ dataParser.cpp
@@ -1,29 +1,48 @@
+/***************************************
+* Affiliation: Oregon State University
+* Author: Gregory Sanchez
+*
+* Course: ECE 271 - Digital Logic Design
+* Project Name: ECE 271 Final Project
+* Team Number: 09
+*
+* Description: Data Parser
+* Start Date: 06/05/2018
+*
+****************************************/
 #include <stdio.h>
-#include <tchar.h>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <iomanip>
 using namespace std;
 
+/*
+*	Function: The CSV data must be copied into a text document
+*							named "test.txt". The code parsers the text document
+*							whether the IR or PS/2 needs to be parsed
+*	Inputs:		text file
+*	Outputs:	Parsed information that can be copied to a new text document
+*
+*
+*/
 int main()
 {
-	ifstream input("test.txt"); //use data from file: "StudentData.txt"
+	ifstream input("test.txt"); //use data from file: "text.txt"
 
 	bool test = true;
-	double time, ch1, ch2; //x is the score
+	double time, ch1, ch2;
 
-	double time0 = 0; 
-	double time1 = 0;
+	double time0 = 0;
 
 	while (!input.eof()) { //keep looping untill end of file
 		/*
-		//Uncomment this section to get the values 
+		//Uncomment this section to get the values
 		//for the IR decoder for the time length for each 0
 		input >> time >> ch2;
 
 		if (ch2 > 3 && test == true)
-		{	
+		{
 			cout << "Time " << time << " of " << ch2 << endl;
 			cout << endl;
 			//cout << "Time difference of 1: " << time << " - 0: " << time0 << " is : " << (time - time0) << endl;
@@ -42,8 +61,6 @@ int main()
 		}
 		*/
 
-
-
 		/*
 		//Uncomment this section to get the values
 		//for the PS2 decoder on each falling edge of the clock
@@ -60,12 +77,7 @@ int main()
 		}
 		*/
 	}
-	
-
-
 	cin.clear();
 	cin.ignore(255, '\n');
 	cin.get();
-
-
 }
